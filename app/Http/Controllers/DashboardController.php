dc8d5c90d1f65aba1c5b9298f471facbd4bd8646
Widiputri12 <widiaudy1204@gmail.com>
2026-05-14 19:57:41 +0700

    tampilan dashboard3

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,51 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request; // <-- WAJIB ADA AGAR BISA MENERIMA FILTER
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
 use App\Models\Service;
-use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
-    public function index(Request $request) // <-- Tambahkan parameter (Request $request)
-    {
-        /** @var \App\Models\User $user */
-        $user = Auth::user();
-        
-        // --- 1. DASHBOARD ADMIN ---
-        if ($user->role === 'admin') {
-            $query = Order::with(['user', 'services', 'technicians']);
-
-            // Logika Filter Tanggal Pengerjaan (booking_date)
-            if ($request->start_date && $request->end_date) {
-                $query->whereDate('booking_date', '>=', $request->start_date)
-                      ->whereDate('booking_date', '<=', $request->end_date);
-            }
-
-            $orders = $query->latest()->paginate(10);
-            
-            // Pertahankan filter saat klik halaman 2, 3, dst (Pagination)
-            $orders->appends($request->all()); 
-
-            return view('dashboard', compact('orders')); // Pastikan nama view-nya sesuai dengan punyamu
+    public function index()
+        {
+            /** @var \App\Models\User $user */
+            $user = Auth::user();
+            if ($user->role === 'admin') {
+            $orders = Order::with(['user', 'services', 'technicians'])->latest()->paginate(10);
+            return view('admin.dashboard', compact('orders')); 
         }
-        
-        // --- 2. DASHBOARD TEKNISI ---
-        if ($user->role === 'technician') {
-            // Tarik relasinya dulu
-            $query = $user->assignedOrders()->with(['user', 'services']);
-
-            // Jika admin/teknisi mengisi filter tanggal
-            if ($request->start_date && $request->end_date) {
-                $query->whereDate('booking_date', '>=', $request->start_date)
-                      ->whereDate('booking_date', '<=', $request->end_date);
+            if ($user->role === 'technician') {
+                $assignedOrders = $user->assignedOrders()->with(['user', 'services'])->latest()->get();
+                return view('technician.dashboard', compact('assignedOrders')); 
             }
-
-            // Ambil datanya
-            $assignedOrders = $query->latest()->get();
-            return view('technician.dashboard', compact('assignedOrders')); 
+            $services = Service::all(); 
+            $myOrders = Order::with('services')
+                        ->where('user_id', $user->id)
+                        ->latest()
+                        ->get();
+            return view('dashboard', compact('services', 'myOrders'));
         }
-}
 }
